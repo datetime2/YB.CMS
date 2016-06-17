5557785ee16312d90c474015f0f381c87437ffb2
KozoWo1aspx <kozo.wo1aspx@gmial.com>
2016-06-17 12:57:06 +0800

    .

---
+++ 01.YB.CMS.Presentation/YB.CMS.Ui/Areas/Manager/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YB.CMS.Ui.Areas.Manager.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Manager/Login
+        public ActionResult Out()
+        {
+            return RedirectToAction("Index", "Manager/Login");
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
