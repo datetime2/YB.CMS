ba80777386f647dd206df1451ab12531a3b55975
KozoWo1aspx <kozo.wo1aspx@gmial.com>
2016-06-17 09:48:06 +0800

    新增工具类

---
+++ 04.YB.CMS.Utility/YB.CMS.Utility/EnumHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+namespace YB.CMS.Utility
+{
+    public static class EnumHelper
+    {
+        private static Hashtable enumDesciption = GetDescriptionContainer();
+
+        private static void AddToEnumDescription(Type enumType)
+        {
+            enumDesciption.Add(enumType, GetEnumDic(enumType));
+        }
+
+        private static string GetDescription(Type enumType, string enumText)
+        {
+            if (string.IsNullOrEmpty(enumText))
+            {
+                return null;
+            }
+            if (!enumDesciption.ContainsKey(enumType))
+            {
+                AddToEnumDescription(enumType);
+            }
+            var obj2 = enumDesciption[enumType];
+            if ((obj2 == null) || string.IsNullOrEmpty(enumText))
+            {
+                throw new ApplicationException("不存在枚举的描述");
+            }
+            var dictionary = (Dictionary<string, string>)obj2;
+            return dictionary[enumText];
+        }
+
+        private static Hashtable GetDescriptionContainer()
+        {
+            enumDesciption = new Hashtable();
+            return enumDesciption;
+        }
+
+        private static Dictionary<string, string> GetEnumDic(Type enumType)
+        {
+            var dictionary = new Dictionary<string, string>();
+            var fields = enumType.GetFields();
+            foreach (var info in fields)
+            {
+                if (info.FieldType.IsEnum)
+                {
+                    var customAttributes = info.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                    dictionary.Add(info.Name, ((DescriptionAttribute)customAttributes[0]).Description);
+                }
+            }
+            return dictionary;
+        }
+
+        private static bool IsIntType(double d)
+        {
+            return (((int)d) != d);
+        }
+
+        public static string ToDescription(this Enum value)
+        {
+            var enumType = value.GetType();
+            var name = Enum.GetName(enumType, value);
+            return GetDescription(enumType, name);
+        }
+
+        public static Dictionary<int, string> ToDescriptionDictionary<TEnum>()
+        {
+            var enumType = typeof(TEnum);
+            var values = Enum.GetValues(enumType);
+            return values.Cast<Enum>().ToDictionary(Convert.ToInt32, enum2 => enum2.ToDescription());
+        }
+
+        public static Dictionary<int, string> ToDictionary<TEnum>()
+        {
+            var enumType = typeof(TEnum);
+            var values = Enum.GetValues(enumType);
+            return values.Cast<Enum>().ToDictionary(Convert.ToInt32, enum2 => enum2.ToString());
+        }
+    }
+}
---
+++ 04.YB.CMS.Utility/YB.CMS.Utility/Log.cs
@@ -0,0 +1,73 @@
+﻿[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace YB.CMS.Utility
+{
+    using log4net;
+    using System;
+    using System.Diagnostics;
+    /// <summary>
+    /// log4net 日志处理
+    /// </summary>
+    public class Log
+    {
+        public static void Debug(object message)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Debug(message);
+        }
+        public static void Debug(object message, Exception ex)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Debug(message, ex);
+        }
+
+        public static void Error(object message)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Error(message);
+        }
+
+        public static void Error(object message, Exception exception)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Error(message, exception);
+        }
+        public static void Info(object message)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Info(message);
+        }
+
+        public static void Info(object message, Exception ex)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Info(message, ex);
+        }
+
+        public static void Warn(object message)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Warn(message);
+        }
+
+        public static void Warn(object message, Exception ex)
+        {
+            LogManager.GetLogger(GetCurrentMethodFullName()).Warn(message, ex);
+        }
+        private static string GetCurrentMethodFullName()
+        {
+            try
+            {
+                StackFrame frame;
+                string str2;
+                var num = 2;
+                var trace = new StackTrace();
+                var length = trace.GetFrames().Length;
+                do
+                {
+                    frame = trace.GetFrame(num++);
+                    str2 = frame.GetMethod().DeclaringType.ToString();
+                }
+                while (str2.EndsWith("Excption") && (num < length));
+                string name = frame.GetMethod().Name;
+                return (str2 + "." + name);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
