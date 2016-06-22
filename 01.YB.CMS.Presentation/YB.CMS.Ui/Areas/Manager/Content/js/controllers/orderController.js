78da487de6dec3a31519ed7373b441fad03fd188
KozoWo1aspx <kozo.wo1aspx@gmial.com>
2016-06-22 17:35:14 +0800

    ng-table 新增数据导出功能

--- 01.YB.CMS.Presentation/YB.CMS.Ui/Areas/Manager/Content/js/controllers/orderController.js
+++ 01.YB.CMS.Presentation/YB.CMS.Ui/Areas/Manager/Content/js/controllers/orderController.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('CmsApp', ['ngTable']).
+﻿var app = angular.module('CmsApp', ['ngTable','ngTableToCsv']).
     controller('orderCtrl', function ($scope, ngTableParams, orderService) {
         var vm = $scope.vm = {
             orderid: "",
